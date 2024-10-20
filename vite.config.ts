73f57d58850b3554c6af776ee01120cc8c7cf339
Magsmags600 <margaretjenkins@gmail.com>
2024-10-20 12:27:21 -0700

    APItest

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   envDir: './env',
+//   plugins: [react()],
+// });
+import { defineConfig } from 'vite';
+
 export default defineConfig({
-  envDir: './env',
-  plugins: [react()],
+  server: {
+    host: 'localhost',
+    port: 3000, // This sets the port for localhost
+  },
 });
